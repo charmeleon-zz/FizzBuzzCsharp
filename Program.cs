fd4191c01e47f38b992f29f7c691da4af9de04c8
Erick Rivas <erick@simplenight.com>
2016-09-12 19:28:31 -0500

    Working Fizz Buzz

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int Counter = 1; Counter <= 100; Counter++)
+            {
+                String BufferString = "";
+                if (0 == Counter % 3) {
+                    BufferString += "Fizz";
+                }
+                if (0 == Counter % 5) {
+                    BufferString += "Buzz";
+                }
+                if (String.IsNullOrEmpty(BufferString)) {
+                    BufferString += Counter.ToString();
+                }
+                Console.WriteLine(BufferString);
+            }
+        }
+    }
+}
